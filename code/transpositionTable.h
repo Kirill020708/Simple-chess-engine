2ad95ffb195cef139ccfc64356494ecce43d1e35
Kirill020708 <kirill.tim.020708@mail.ru>
2025-09-30 22:02:57 +0300

    TT bug

--- code/transpositionTable.h
+++ code/transpositionTable.h
@@ -58,7 +58,7 @@ struct TranspositionTable{
 		if(table[index].key!=key)
 			return {NO_EVAL,Move()};
 		if(table[index].depth<depth)
-			return {NO_EVAL,Move()};
+			return {NO_EVAL,table[index].bestMove};
 
 		int eval=NO_EVAL;
 		if(table[index].type==EXACT)
