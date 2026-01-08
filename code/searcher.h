efd24c67db847ff3ea295a5b9e83ddd68cae77f2
Kirill020708 <kirill.tim.020708@mail.ru>
2026-01-08 08:50:19 +0300

    fixed a bug with detecting non-quiet TT move in LMR
    bench 1018033

--- code/searcher.h
+++ code/searcher.h
@@ -572,10 +572,8 @@ struct Worker {
                 int LMR_DEPTH_REDUCTION =
                     floor(log(float(depth)) * log(float(movesSearched)) / 3 + 0.5 -
                           1 * (isPvNode)-1.5 * float(historyValue) / historyHelper.maxHistoryScore +
-                          0.5 * (!improving)); // reduction of depth
+                          0.5 * (!improving) + (isTTCapture) * 1); // reduction of depth
 
-                if (ttMove != Move() && !board.isQuietMove(ttMove))
-                    LMR_DEPTH_REDUCTION++;
                 if (LMR_DEPTH_REDUCTION < 0)
                     LMR_DEPTH_REDUCTION = 0;
 
